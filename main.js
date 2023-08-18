4e39f611e3935d22bd258cbe59c192f885e52cf8
Zambeli <zambelidev@gmail.com>
2023-08-17 22:03:14 -0300

    finalizando

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
+const sobre = document.querySelector('.animacao')
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
-openMenu.addEventListener('touchstart',show)
-closeMenu.addEventListener('touchstart',close)
 
 menu_items.forEach(item => {
     item.addEventListener('click',function(){
-        close();
+        if (window.matchMedia("(max-width: 600px)").matches) {
+            close();
+          };
     })
 })
 
@@ -18,7 +19,20 @@ function show(){
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
 }
+
 function close(){
     mainMenu.style.top = '-100%';
     mainMenu.style.display = 'none'
 }
+
+function ativarLetra (elemento) {
+    const arrTexto = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    arrTexto.forEach((letra, i ) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra 
+        }, 60 * i)
+    })
+}
+
+ativarLetra(sobre)
